658581b0a74061b2000b257d30df7eb53572e960
denballakh <denis.ballakh@gmail.com>
2025-06-15 03:49:21 +0300

    add string builder module

---
+++ sb.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+import typing as t
+import collections.abc as abc
+import dataclasses
+
+
+@dataclasses.dataclass(slots=True, frozen=True)
+class _Node:
+    val: str
+    next: _Node | None = None
+
+    def __iter__(self) -> abc.Iterator[str]:
+        node = self
+        while node is not None:
+            yield node.val
+            node = node.next
+
+
+class StringBuilder:
+    __slots__ = ('_node',)
+    _node: _Node
+
+    def __init__(self, _node: _Node = _Node('', None)) -> None:
+        self._node = _node
+
+    def add(self, s: str) -> t.Self:
+        self._node = _Node(s, self._node)
+        return self
+    
+    def __iadd__(self, s: str) -> t.Self:
+        return self.add(s)
+
+    def copy(self) -> t.Self:
+        return self.__class__(self._node)
+
+    def __getitem__(self, index: slice) -> t.Self:
+        if index != slice(None):
+            raise ValueError(f'only [::] slice allowed')
+        return self.copy()
+
+    @t.override
+    def __str__(self) -> str:
+        s = ''.join(reversed([*self._node])) # is there a better way?
+        self._node = _Node(s)
+        return s
+
+def example() -> None:
+    # create:
+    s = StringBuilder() 
+    # append:
+    s += 'chunk'
+    s.add('chunk')
+    # fork:
+    s2 = s.copy()
+    s2 = s[::]
+    # build:
+    str(s)
